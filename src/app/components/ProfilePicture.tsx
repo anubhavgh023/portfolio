7c4ce31ce045dbd0ddf04bc51ff7677b3df3ad45
Anubhav <anubhavsamanta023@gmail.com>
2024-06-08 11:55:16 +0530

    Fix: width, height & border color

--- src/app/components/ProfilePicture.tsx
+++ src/app/components/ProfilePicture.tsx
@@ -5,8 +5,9 @@ export default function ProfilePicture() {
       <Image
         src="/profile.jpg"
         width={300}
-        height={100}
+        height={300}
         alt="Picture of the author"
+        className="border border-slate-500 m-5 rounded-md"
       ></Image>
   );
 }
