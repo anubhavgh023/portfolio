c747a04464013d667e3c6ebc1c63d2af1986104e
Anubhav <anubhavsamanta023@gmail.com>
2024-06-09 12:22:13 +0530

    minor fixs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,24 +7,30 @@ import TechStack from "../components/TechStack";
 export default function Home() {
   return (
     <div className="m-5 flex flex-col gap-10">
-      <div className="grid grid-cols-6 gap-5">
-        <span className="col-span-1">
+      <div className="grid grid-cols-1 lg:grid-cols-6 gap-5">
+        <span className="col-span-1 lg:col-span-1">
           <ProfilePicture></ProfilePicture>
         </span>
-        <span className="col-span-5">
+        <span className="col-span-1 lg:col-span-5">
           <AboutMe></AboutMe>
         </span>
 
-        <span className="col-span-1">
+        {/* only visible on lg-screens */}
+        <span className="col-span-1 hidden lg:block">
           <Socials></Socials>
         </span>
-        <span className="col-span-5">
+
+        <span className="col-span-1 lg:col-span-5">
           <TechStack></TechStack>
         </span>
       </div>
       <div>
         <ProjectSection></ProjectSection>
       </div>
+        {/* only visible on sm-screens */}
+        <span className="col-span-1 block lg:hidden">
+          <Socials></Socials>
+        </span>
     </div>
   );
 }
